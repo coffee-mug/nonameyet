e6d6e2d2ecf82fe03603b7832fb8696cce0e150b
Lucas Kostka <lucas.kostka@escdijon.eu>
2018-08-09 18:49:30 +0200

    Fix styling + added possibility to send message

--- server.js
+++ server.js
@@ -3,54 +3,53 @@ const WebSocket = require('ws')
 const fs = require('fs')
 const path = require('path')
 
-
-const STATIC_FOLDER = "/"
+const STATIC_FOLDER = '/'
 
 const server = new http.createServer((req, res) => {
-    var sendThrough;
-    switch(true){
-        case req.url === "/":
-            sendThrough = fs.createReadStream('./index.html')
-            sendThrough.pipe(res)
-            break;
-        case /\.css$|\.js$|\.html$/.test(req.url):
-            try {
-                var lookPath = path.join(__dirname, STATIC_FOLDER, req.url.split('/').slice(-1)[0])
-                console.log("Lookign for file at %s", lookPath)
-                sendThrough = fs.createReadStream(lookPath)
-                sendThrough.pipe(res)
-                break;
-            } catch (e) {
-                console.error(e)
-                res.statusCode =  500
-                res.end()
-            }
-        default:
-            res.statusCode = 404
-            res.end()
-
-    }
-    console.log(req.url)
+  var sendThrough
+  switch (true) {
+    case req.url === '/':
+      sendThrough = fs.createReadStream('./index.html')
+      sendThrough.pipe(res)
+      break
+    case /\.css$|\.js$|\.html$/.test(req.url):
+      try {
+        var lookPath = path.join(__dirname, STATIC_FOLDER, req.url.split('/').slice(-1)[0])
+        console.log('Lookign for file at %s', lookPath)
+        sendThrough = fs.createReadStream(lookPath)
+        sendThrough.pipe(res)
+        break
+      } catch (e) {
+        console.error(e)
+        res.statusCode = 500
+        res.end()
+        break
+      }
+    default:
+      res.statusCode = 404
+      res.end()
+  }
+  console.log(req.url)
 })
 const wss = new WebSocket.Server({server})
 
-let lastReceived = Date.now();
+let lastReceived = Date.now()
 
-wss.on('connection', function connection(ws){
-    console.log("YOLOO new conn")
-    ws.on('message', function incoming(message){
-        // naive request rate limiting
-        if (lastReceived && (Date.now() - lastReceived) > 5 * 1000) {
-            console.log('A new video to share has been received %s', message)
+wss.on('connection', function connection (ws) {
+  console.log('YOLOO new conn')
+  ws.on('message', function incoming (message) {
+    // naive request rate limiting
+    if (lastReceived && (Date.now() - lastReceived) > 5 * 1000) {
+      console.log('A new video to share has been received %s', message)
 
-            wss.clients.forEach( (client) => {
-                if (client !== ws && client.readyState === WebSocket.OPEN) {
-                    client.send(message)
-                }
-            })
+      wss.clients.forEach((client) => {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(message)
         }
-        lastReceived = Date.now()
-    })
+      })
+    }
+    lastReceived = Date.now()
+  })
 })
 
 server.listen(8000)
--- youtubeManager.js
+++ youtubeManager.js
@@ -1,121 +1,158 @@
-(function() {
-    var state;
+(function () {
+  var state
 
-    var playerController = {
-        init() {
-            this.connect();
-            this.setupDOM();
-            this.setupListener();
-            window.onYouTubeIframeAPIReady = () => {
-                this.onYouTubeIframeAPIReady.call(this);
-            }
-        },
-        setupDOM() {
-            var root = document.createElement('div');
-            root.id = "player";
-            document.body.appendChild(root);
+  var playerController = {
+    init () {
+      this.connect()
+      this.setupDOM()
+      this.setupListener()
+      window.onYouTubeIframeAPIReady = () => {
+        this.onYouTubeIframeAPIReady.call(this)
+      }
+    },
+    setupDOM () {
+      var formVideo = document.getElementById('form-video')
+      var root = document.createElement('div')
+      root.id = 'player'
+      formVideo.parentNode.insertBefore(root, formVideo) 
 
-            // 2. This code loads the IFrame Player API code asynchronously.
-            var tag = document.createElement('script');
+      // 2. This code loads the IFrame Player API code asynchronously.
+      var tag = document.createElement('script')
 
-            tag.src = "https://www.youtube.com/iframe_api";
-            var firstScriptTag = document.getElementsByTagName('script')[0];
-            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-        },
-        onYouTubeIframeAPIReady() {
+      tag.src = 'https://www.youtube.com/iframe_api'
+      var firstScriptTag = document.getElementsByTagName('script')[0]
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+    },
+    onYouTubeIframeAPIReady () {
+      window.player = new YT.Player('player', {
+        height: '390',
+        width: document.documentElement.clientWidth.toString(),
+        videoId: 'M7lc1UVf-VE',
+        events: {
+          'onReady': this.onPlayerReady
+        }
+      })
+    },
+    flash (message, type) {
+      var domPlaceHolder = document.getElementById('flash')
+      domPlaceHolder.textContent = message
 
-            console.log(this)
+      switch (type) {
+        case 'error':
+          domPlaceHolder.style.color = 'red'
+          break
+        default:
+          break
+      }
 
-            window.player = new YT.Player('player', {
-                height: '390',
-                width: '640',
-                videoId: 'M7lc1UVf-VE',
-                events: {
-                    'onReady': this.onPlayerReady,
-                }
-            });
-        },
-        validate(input) {
-            var output = "";
+      // Fade it after 5 sec
+      var to = setTimeout(() => {
+        domPlaceHolder.textContent = ''
+        clearTimeout(to)
+      }, 5 * 1000)
+    },
+    validate (input) {
+      var output = ''
 
-            switch(true){
-                case /^https:\/\/www\.youtube\.com/.test(input):
-                    // normal url, id is in the "v" query param
-                    output = input.split("v=")[1]
+      switch (true) {
+        case /^https:\/\/www\.youtube\.com/.test(input):
+          // normal url, id is in the "v" query param
+          output = input.split('v=')[1]
 
-                    if (!output) {
-                        // May be an url extracted from the iframe embed code, let's try
-                        // something else
-                        output = /embed/.test(input) ? input.split('/').slice(-1)[0] : "";
-                    }
-                    break
-                case /^https:\/\/youtu\.be/.test(input):
-                    output = input.split('/').slice(-1)[0];
-                    break;
-                // Curious enough to try ?
-                case input === "banana":
-                    output = "h3ilLEN1Qew"
-                    break;
-                // Want to add a new video to the shortcut list ? 
-                // Feel welcome to shoot me your wish on https://twitter.com/lucaskostka
-                // I'll do my best to add it quick.
-                default:
-                    output = "aDm5WZ3QiIE"
-            }
+          if (!output) {
+            // May be an url extracted from the iframe embed code, let's try
+            // something else
+            output = /embed/.test(input) ? input.split('/').slice(-1)[0] : ''
+          }
+          break
+        case /^https:\/\/youtu\.be/.test(input):
+          output = input.split('/').slice(-1)[0]
+          break
+          // Curious enough to try ?
+        case input === 'banana':
+          output = 'h3ilLEN1Qew'
+          break
+          // Want to add a new video to the shortcut list ?
+          // Feel welcome to shoot me your wish on https://twitter.com/lucaskostka
+          // I'll do my best to add it quick.
+        default:
+          this.flash('Invalid URL, please submit a URL of the form "https://www.youtube.com/watch?" or "https://youtu.be/f4Mc-NYPHaQ"', 'error')
+          output = 'aDm5WZ3QiIE'
+      }
 
-            return output;
-        },
-        onPlayerReady: function onPlayerReady(event) {
-            event.target.playVideo();
+      return output
+    },
+    onPlayerReady: function onPlayerReady (event) {
+      event.target.playVideo()
 
-            this.state = {
-                set videoId(newVideoId) {
-                    try {
-                        window.player.loadVideoById(newVideoId)
-                    } catch(e) {
-                        console.error(e)
-                    }
-                }
-            }
-        },
-        setupListener() {
-            var updateForm = document.getElementById('form-video');
-            var lastTimeSent = Date.now();
+      this.state = {
+        set videoId (newVideoId) {
+          try {
+            window.player.loadVideoById(newVideoId)
+          } catch (e) {
+            console.error(e)
+          }
+        }
+      }
+    },
+    setupListener () {
+      var updateForm = document.getElementById('form-video')
+      var lastTimeSent = Date.now()
 
-            updateForm.addEventListener('submit', function(e) {
-                e.preventDefault()
+      updateForm.addEventListener('submit', function (e) {
+        e.preventDefault()
 
-                var newVideo = document.getElementById('input-video')
+        var newVideo = document.getElementById('input-video')
+        var message = document.getElementById('input-message')
+        var now = new Date()
 
-                var vidId = this.validate(newVideo.value);
+        message = message.value.length < 255 ? message.value : message.value.slice(0, 255)
 
-                window.state.videoId = vidId;
+        // Set the message on the page
+        document.getElementById('message').textContent = [now.getHours(), now.getMinutes()].join(':') + ' | ' + message
 
-                // naive request spamming prevention
-                if ( (Date.now() - lastTimeSent) > 5 * 1000) {
-                    console.log("Allowed to share another video")
-                    this.ws.send(vidId);
-                }
+        var vidId = this.validate(newVideo.value)
 
-                lastTimeSent = Date.now();
-                //  ah ah, DONE !
-            }.bind(this))
-        },
-        connect() {
-            var protocol = document.location.protocol === "https:" ? "wss" : "ws"
-            var port = document.location.port
+        window.state.videoId = vidId
 
-            this.ws = new WebSocket(protocol + "://" + document.location.hostname + (port ? ":" + port : port))
+        // naive request spamming prevention
+        if ((Date.now() - lastTimeSent) > 5 * 1000) {
+          console.log('Allowed to share another video')
 
-            this.ws.onmessage = (video) => {
-                console.log("Received a new video to display", video.data)
+          var payload = { message: message, vidId: vidId }
+          this.ws.send(JSON.stringify(payload))
+        }
 
-                if (video.data !== window.state.videoId) {
-                    window.state.videoId = video.data;
-                }
-            }
-        },
+        lastTimeSent = Date.now()
+        //  ah ah, DONE !
+      }.bind(this))
+    },
+    connect () {
+      var protocol = document.location.protocol === 'https:' ? 'wss' : 'ws'
+      var port = document.location.port
+
+      this.ws = new WebSocket(protocol + '://' + document.location.hostname + (port ? ':' + port : port))
+
+      this.ws.onmessage = (payload) => {
+        console.log('Received a new payload to display', payload)
+
+        var parsedPayload = JSON.parse(payload.data)
+
+        console.log(payload)
+
+        // Display video if different than current one
+        if (parsedPayload.vidId !== window.state.videoId) {
+          window.state.videoId = payload.vidId
+        }
+
+        // And display th emessage if not-null
+        if (parsedPayload.message) {
+          var now = new Date()
+          document.getElementById('message').textContent = [now.getHours(), now.getMinutes()].join(':') + ' | ' + parsedPayload.message
+        }
+      }
     }
+  }
 
-    playerController.init();
-})();
+  playerController.init()
+})()
